44b0b5fb0396863c3ddda428410e0d812636000c
Hnicky <hour.nicky@gmail.com>
2018-05-29 17:16:54 +0200

    First commit

---
+++ animals.js
@@ -0,0 +1,39 @@
+function lionSound(){
+  var sound = document.getElementById("lion");
+  sound.play();
+}
+
+function elephantSound(){
+  var sound = document.getElementById("elephant");
+  sound.play();
+}
+
+function singeSound(){
+  var sound = document.getElementById("singe");
+  sound.play();
+}
+
+function leopardSound(){
+  var sound = document.getElementById("leopard");
+  sound.play();
+}
+
+function rhinosSound(){
+  var sound = document.getElementById("rhinoceros");
+  sound.play();
+}
+
+function hippoSound(){
+  var sound = document.getElementById("hippopotame");
+  sound.play();
+}
+
+function toucanSound(){
+  var sound = document.getElementById("toucan");
+  sound.play();
+}
+
+function crocoSound(){
+var sound = document.getElementById("crocodile")
+sound.play();
+}
